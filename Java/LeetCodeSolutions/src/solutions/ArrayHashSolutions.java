d97bd9385dea970dac2dd7780e5c106d8ffbeca8
Snowager <colepr24@gmail.com>
2024-08-17 15:19:41 -0600

    Add 2Sum solution (java)

--- Java/LeetCodeSolutions/src/solutions/ArrayHashSolutions.java
+++ Java/LeetCodeSolutions/src/solutions/ArrayHashSolutions.java
@@ -1,5 +1,22 @@
 package solutions;
 
+import java.util.HashMap;
+
 public class ArrayHashSolutions {
 
+    /* Params: unsorted array nums, int target 
+     * Return: 2 size int array of number pair that equals target */
+    public static int[] twoSum(int[] nums, int target) {
+		final int[] twoNum = new int[2]; // fixed array with final values to optimize
+		HashMap<Integer, Integer> numMap = new HashMap<Integer, Integer>(); // Hashmap for grouping
+		for (int x = 0; x < nums.length; x++) {
+			if (!numMap.containsKey(target - nums[x])) {
+				numMap.put(nums[x], x); 
+			} else {
+				twoNum[1] = x;
+				twoNum[0] = numMap.get(target - nums[x]);
+			}
+		}
+		return twoNum;
+	}
 }
