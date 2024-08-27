f4d90171ffa81a0496b12366ee56fea7e41adfe4
Snowager <colepr24@gmail.com>
2024-08-27 09:44:07 -0600

    Add solution comments p.49

--- Javascript/arrayMapSolutions.js
+++ Javascript/arrayMapSolutions.js
@@ -6,16 +6,19 @@
 function groupAnagram(strArr) {
     let groupAnagramArr = [];
     let anagramMap = new Map();
+    // for each string, save sorted form as map key
     for (let str of strArr) {
       let sortedString = str.split("").sort().join("");
       if (!anagramMap.has(sortedString)) {
           anagramMap.set(sortedString, [str]);
       } else {
         let tempArr = anagramMap.get(sortedString);
+        // push matching strings to saved array
         anagramMap.set(sortedString, [...tempArr, str]);
       }
     }
     for (let key of anagramMap.keys()) {
+        // push all key/value arrays to final result array
         groupAnagramArr.push(anagramMap.get(key));
     }
     return groupAnagramArr;
