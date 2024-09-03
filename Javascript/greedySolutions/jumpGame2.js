e32fbe52e772a10a833e36f32dd0933d80bc1ecf
Snowager <colepr24@gmail.com>
2024-09-03 06:43:33 -0600

    Update formatting

--- Javascript/greedySolutions/jumpGame2.js
+++ Javascript/greedySolutions/jumpGame2.js
@@ -8,13 +8,16 @@ var jump = function(nums) {
     if (nums.length === 0) return undefined;
     if (nums.length === 1) return 0;
     if (nums.length === 2) return 1;
+
     let l = 0, r = 0;
     let pos = 0;
     let jumps = 0;
+
     // while we haven't traversed array
     while (r < nums.length - 1) {
         // pos tracks max pos we can jump to from our location
         pos = 0;
+        
         for (let x = l; x < r+1; x++) {
             // let our pos be the max pos from where we can jump next
             pos = Math.max(pos, nums[x]+x);
